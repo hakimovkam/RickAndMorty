30a2a1f11c9921692d24663bf8c9f5111661b4b8
Камиль Хакимов <hakimov.kam@yandex.ru>
2023-01-04 12:59:56 +0300

    Added first screen with tableView and SearchBar

---
+++ RickAndMorty/CharacterManager.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterManager.swift
+//  RickAndMorty
+//
+//  Created by Камиль Хакимов on 04.01.2023.
+//
+
+import Foundation
+
+struct CharacterManager {
+//    let URL = 
+}
--- RickAndMorty/ViewController.swift
+++ RickAndMorty/ViewController.swift
@@ -9,11 +9,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let data = ["1", "2", "3", "4"]
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    let searchBar = UISearchBar()
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+                
+        setupViews()
+        setConstraints()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: "TableView")
 
+    }
 
 }
 
+class TableViewCell: UITableViewCell {
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.accessoryType = .none
+    }
+}
+
+//MARK: - extension
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { return data.count }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableView", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
+extension ViewController {
+    
+    func setupViews() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchController
+        navigationItem.title = "Rick and Morty"
+
+        
+        view.addSubview(searchBar)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(tableView)
+        tableView.backgroundColor = .white
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+            
+        ])
+    }
+}
